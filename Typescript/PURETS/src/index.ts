7341f82b44660525375fc72c95c6b291fd2e4dea
somnath0186 <124126518+somnath0186@users.noreply.github.com>
2023-05-24 18:05:08 +0530

    second commit

--- Typescript/PURETS/src/index.ts
+++ Typescript/PURETS/src/index.ts
@@ -1,13 +1,34 @@
-class User {
-     name: string;
-     age: number;
-   readonly city: string="pune";
-    constructor(name:string, age:number) {
-            this.name = name;
-            this.age = age;
-            // this.city=city;
-        }
-}
+// class User {
+//      name: string;
+//      age: number;
+//    readonly city: string="pune";
+//     constructor(name:string, age:number) {
+//             this.name = name;
+//             this.age = age;
+//             // this.city=city;
+//         }
+// }
 
-// const user=new User('somnath',23);
-// console.log(user);
+class User {
+  private _courseCount: number = 10;
+
+  constructor(public name: string, public age: number, public email: string) {}
+  private deleteToken(){
+    return "tokenmm is deleted"
+  }
+  get getCourseCount(): number {
+    return this._courseCount;
+  }
+  set setCourseCount(value: number) {
+    if (value < 1) {
+      throw new Error("Course count cannot be less than 1");
+    } else {
+      this._courseCount = value;
+    }
+  }
+}
+let hanuman = new User('somnath',23,'somn');
+hanuman.setCourseCount=555
+let value=hanuman.getCourseCount;
+
+console.log(value);
